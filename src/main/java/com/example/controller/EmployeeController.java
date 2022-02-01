b666dd178c8c0a32a1e291a8a6cf68fcafeb59ab
admin <admin@admin-PC>
2022-02-01 15:44:03 +0900

    🎨コントローラ：必要なメソッドまわりで動きに異常がないことを確認

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -29,12 +29,20 @@ public class EmployeeController {
     public String getList (Model model) {
         // JpaRepositoryのメソッドであるfindAll()を使ってDBに保存されているemployeeのデータを持ってくる
         model.addAttribute ("employees", repository.findAll ());
-        
+        System.out.println ("社員数は" + repository.findAll ().size ());
         return "list";
     }
     
-    @GetMapping ("regist")
+    @GetMapping ("/regist")
     public String getRegistEmployee (@ModelAttribute Employee employee, Model model) {
+        // System.out.println ("year" + employee.getBirth_year ());
+        // System.out.println ("month" + employee.getBirth_month ());
+        // System.out.println ("day" + employee.getBirth_day ());
+        if (employee.getGender () == null) {
+            // System.out.print (employee.getGender ());
+            employee.setGender ("男");
+            // System.out.print (employee.getGender ());
+        }
         model.addAttribute ("genderList", gGenderList.getGender ());
         model.addAttribute ("birthYear", gBirthList.getBirthYear ());
         model.addAttribute ("birthMonth", gBirthList.getBirthMonth ());
@@ -46,8 +54,13 @@ public class EmployeeController {
     }
     
     // actionで受けとる
-    @PostMapping ("/registed")
+    @PostMapping ("/regist")
     public String registEmployee (@Validated @ModelAttribute Employee employee, BindingResult result, Model model) {
+        System.out.println ("year" + employee.getBirth_year ());
+        System.out.println ("month" + employee.getBirth_month ());
+        System.out.println ("day" + employee.getBirth_day ());
+        // System.out.println (employee.getGender ());
+        
         if (result.hasErrors ()) {
             model.addAttribute ("genderList", gGenderList.getGender ());
             model.addAttribute ("birthYear", gBirthList.getBirthYear ());
@@ -56,16 +69,27 @@ public class EmployeeController {
             model.addAttribute ("joiningYear", gJoiningDateList.getJoinYear ());
             model.addAttribute ("joiningMonth", gJoiningDateList.getJoinMonth ());
             model.addAttribute ("joiningDay", gJoiningDateList.getJoinDay ());
+            System.out.print ("エラー出てる");
             return "regist";
         }
         
         repository.save (employee);
-        return "list";
+        return "redirect:/";
     }
     
-    @GetMapping ("/edit{id}")
-    public String getEditEmployee (@PathVariable Long id, Model model) {
-        model.addAttribute ("employee", repository.findById (id));
+    @GetMapping ("/edit/{user_id}")
+    public String getEditEmployee (@PathVariable Long user_id, Model model) {
+        model.addAttribute ("employee", repository.findById (user_id));
+        
+        System.out.println ("year" + repository.findById (user_id).get ().getBirth_year ());
+        System.out.println ("month" + repository.findById (user_id).get ().getBirth_month ());
+        System.out.println ("month" + repository.findById (user_id).get ().getBirth_day ());
+        model.addAttribute ("birth_year", repository.findById (user_id).get ().getBirth_year ());
+        model.addAttribute ("birth_month", repository.findById (user_id).get ().getBirth_month ());
+        model.addAttribute ("birth_day", repository.findById (user_id).get ().getBirth_day ());
+        model.addAttribute ("jdate_year", repository.findById (user_id).get ().getJdate_year ());
+        model.addAttribute ("jdate_month", repository.findById (user_id).get ().getJdate_month ());
+        model.addAttribute ("jdate_day", repository.findById (user_id).get ().getJdate_day ());
         
         model.addAttribute ("genderList", gGenderList.getGender ());
         model.addAttribute ("birthYear", gBirthList.getBirthYear ());
@@ -82,12 +106,25 @@ public class EmployeeController {
     @PostMapping ("/edited")
     public String editEmployee (@Validated @ModelAttribute Employee employee, BindingResult result, Model model) {
         if (result.hasErrors ()) {
+            model.addAttribute ("genderList", gGenderList.getGender ());
+            model.addAttribute ("birthYear", gBirthList.getBirthYear ());
+            model.addAttribute ("birthMonth", gBirthList.getBirthMonth ());
+            model.addAttribute ("birthDay", gBirthList.getBirthDay ());
+            model.addAttribute ("joiningYear", gJoiningDateList.getJoinYear ());
+            model.addAttribute ("joiningMonth", gJoiningDateList.getJoinMonth ());
+            model.addAttribute ("joiningDay", gJoiningDateList.getJoinDay ());
             
             return "edit";
         }
         
         repository.save (employee);
         return "list";
+    }
+    
+    @GetMapping ("/delete/{id}")
+    public String deleteEmployee (@PathVariable Long id) {
+        repository.deleteById (id);
+        return "redirect:/";
     }
     
 }
