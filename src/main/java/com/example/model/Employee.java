5fbed4ea2cadb3d34c95a31e2c62314281ce66d2
admin <admin@admin-PC>
2022-02-01 16:45:03 +0900

    🎨エラーが出た際の警告文を変更

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -14,43 +14,37 @@ import lombok.Data;
 public class Employee {
     // 社員番号
     @Id
-    @NotNull (message = "空白は許可されていません")
+    @NotNull (message = "必須入力です")
     private Long user_id;
     // 氏名
-    @NotBlank
+    @NotBlank (message = "必須入力です")
     private String user_name;
     // Email
-    @NotBlank
+    @NotBlank (message = "必須入力です")
+    @Pattern (regexp = "^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\\.)+[a-zA-Z]{2,}$", message = "メールアドレス形式で入力してください")
     private String email;
     // 電話番号
     // 半角でハイフン無し
     @Pattern (regexp = "0\\d{9,10}", message = "電話番号の形式でハイフンを含めず半角数字だけで入力してください")
-    @NotBlank
+    @NotBlank (message = "必須入力です")
     private String phone_number;
     // 性別
     // ラジオボタン
-    @NotBlank
+    // 初期値が選択されているのでNotBlankを削除
     private String gender;
     // セレクトボックス
     // 生年月日：年、月、日で入力分けている
-    // @NotBlank
+    // 初期値が選択されているのでNotBlankを削除
     private int birth_year;
-    // @NotBlank
     private int birth_month;
-    // @NotBlank
     private int birth_day;
     // 入社日：生年月日と同じ仕様
-    // @NotBlank
     private int jdate_year;
-    // @NotBlank
     private int jdate_month;
-    // @NotBlank
     private int jdate_day;
     // コメント
     // 最大値を１００文字制限
-    // @NotBlank
-    // @Max (100)
-    // @Min (1)
-    @Size (min = 1, max = 100)
+    
+    @Size (min = 1, max = 100, message = "コメントは1文字以上100文字以下で入力してください")
     private String comment;
 }
