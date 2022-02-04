aee7d9c065be0955e22c26841d318ab3f89d2974
admin <admin@admin-PC>
2022-02-04 17:12:45 +0900

    ⚡️クエリ検索を実装（途中）

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -1,21 +1,34 @@
 package com.example.repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.model.Employee;
 
 // @Repositoryを付けてもいいが、JpaRepositoryを継承しているので省略してもいい
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
+public interface EmployeeRepository extends JpaRepository<Employee, String>, JpaSpecificationExecutor<Employee> {
     
-    List<Employee> findByUserName (String userName);
+    @Query (value = "select * from employee where user_name Like =?1 AND  gender = ?2 AND =?3 < joinday AND joinday < =?4 ", nativeQuery = true)
+    List<Employee> search (String userName, String gender, String joinDateFrom, String joinDateTo);
     
-    List<Employee> findBygender (String gender);
+    @Query (value = "select * from employee where user_name Like  ?1", nativeQuery = true)
+    List<Employee> namesearch (String name);
     
-    List<Employee> findByJoinday (LocalDate joinday);
+    @Query (value = "select * from employee where gender = ?1 ", nativeQuery = true)
+    List<Employee> gendersearch (String gender);
+    
+    @Query (value = "select * from employee where  =?1 < joinday AND joinday < =?2 ", nativeQuery = true)
+    List<Employee> dayearch (String joinDateFrom, String joinDateTo);
+    
+    // List<Employee> findByUserNameLike (String userName);
+    //
+    // List<Employee> findBygenderLike (String gender);
+    //
+    // List<Employee> findByJoinday (LocalDate joinday);
     
 }
