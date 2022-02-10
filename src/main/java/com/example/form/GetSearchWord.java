89a9610bfd6fb0f54d558f51800b2fc1d3f2d759
admin <admin@admin-PC>
2022-02-10 12:46:50 +0900

    🎨DateからStringへ修正

--- src/main/java/com/example/form/GetSearchWord.java
+++ src/main/java/com/example/form/GetSearchWord.java
@@ -1,13 +1,12 @@
 package com.example.form;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class GetSearchWord {
+    
     private String name;
     private String gender;
-    private Date getJoinDateFrom;
-    private Date getJoinDateTo;
+    private String getJoinDateFrom;
+    private String getJoinDateTo;
 }
