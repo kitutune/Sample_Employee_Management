1dc8f4cc0fca854ad3d4bfcaafd94f3ed634003f
admin <admin@admin-PC>
2022-02-16 15:46:38 +0900

    🎨部門ごとのリストを取ってこれるSQLを作成

--- src/main/java/com/example/repository/DemployeeRepository.java
+++ src/main/java/com/example/repository/DemployeeRepository.java
@@ -1,9 +1,18 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.model.Demployee;
 
 public interface DemployeeRepository extends JpaRepository<Demployee, Long> {
+    
+    @Query (value = "SELECT * FROM demployee WHERE user_id = ?1", nativeQuery = true)
+    public Demployee findByUserid (Long id);
+    
+    @Query (value = "SELECT * FROM demployee WHERE department_id = ?1", nativeQuery = true)
+    public List<Demployee> findByDepartmentsList (Long id);
     
 }
